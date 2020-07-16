f3e5adb9ef106810e02b34d5386b834bd4bbadc2
Alejandro Cacheiro González <a.cacheiro69@gmail.com>
2020-07-16 17:10:18 +0200

    Inicio proyecto

---
+++ src/main/java/com/acg/apiRestShop/ApiRestShopApplication.java
@@ -0,0 +1,13 @@
+package com.acg.apiRestShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/acg/apiRestShop/ApiRestShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acg.apiRestShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
